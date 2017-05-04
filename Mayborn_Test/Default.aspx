5a311c6581b215f028d72dfacf6cdfcee31b2685
Layfield <daniel.layfield@hpe.com>
2017-05-04 10:33:56 +0100

    Add project files.

---
+++ Mayborn_Test/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Mayborn_Test.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtFirst" runat="server" Text="Enter First Name"></asp:TextBox>
+            <asp:TextBox ID="txtLast" runat="server" Text="Enter Last Name"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+        </div>
+        <div>
+            <asp:TextBox ID="txtOutput" runat="server" Text="" ReadOnly="true" TextMode="MultiLine"></asp:TextBox>
+        </div>
+    </form>
+
+</body>
+</html>
---
+++ Mayborn_Test/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Mayborn_Test
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack) // sets a condition for if page is not postback
+            {
+
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try // good pratice to do a try catch
+            {
+                // set the user entered contect to string variable so it can be used multiple times
+                string strFirstName = txtFirst.Text; 
+                string strLastName = txtLast.Text;
+                string strFullName = strFirstName + " " + strLastName; // full name will be used for the outputs
+
+                txtOutput.Text = strFullName; // text output box equals full name
+                Directory.CreateDirectory("C:\\MaybornTest\\Output\\"); // create an output directory
+                File.AppendAllText("C:\\MaybornTest\\Output\\Name.txt", strFullName); // append the output to a file. Append keeps all data with out deleting previous data
+            }
+            catch(Exception ex) //capture the expection 
+            {
+                string strDateTime = DateTime.Now.ToLongDateString().ToString() + "-" + DateTime.Now.ToLongTimeString().ToString(); // capture the time of the exception
+                strDateTime = strDateTime.Replace(":", ""); // make the name directory friendly
+                Directory.CreateDirectory("C:\\MaybornTest\\Logs"); // create a log directory
+                File.AppendAllText("C:\\MaybornTest\\Logs\\logfile " + strDateTime +".txt", ex.ToString()); // put the exception in a log file, although unlikely append is used in case more than one expception occurs in the same second
+            }
+        }
+    }
+}
